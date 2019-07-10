3429b590cd81add892b8b88f4451fd4c25f04bbf
Hossam Hamada <hossam.fawzy96@gmail.com>
2019-07-10 19:21:20 +0200

    Updated . notation to define memeTextAttributes

--- MemeMeV1/MemeMeController.swift
+++ MemeMeV1/MemeMeController.swift
@@ -25,10 +25,10 @@ UINavigationControllerDelegate{
     //MARK:- Constants
     let textFieldsDelegate = TextFieldsDelegate()
     let memeTextAttributes: [NSAttributedString.Key: Any] = [
-        NSAttributedString.Key.strokeColor: UIColor.black,
-        NSAttributedString.Key.foregroundColor: UIColor.white,
-        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBold", size: 40)!,
-        NSAttributedString.Key.strokeWidth:  NSNumber(-3.0)]
+        .strokeColor: UIColor.black,
+        .foregroundColor: UIColor.white,
+        .font: UIFont(name: "HelveticaNeue-CondensedBold", size: 40)!,
+        .strokeWidth:  NSNumber(-3.0)]
     
     //MARK:- Variables
     var memedImage: UIImage!
